2db0e92162cfffb76750b2d3d0f8d333fc45488f
Diego Mota <diegoalves123@gmail.com>
2019-04-11 20:08:26 -0300

    Aula 23. Conversão : Usando conversores customiConversão : Usando conversores com selectOneMenuzados

---
+++ src/main/java/br/com/diegomota/cursojsf/dominio/Cidade.java
@@ -0,0 +1,46 @@
+package br.com.diegomota.cursojsf.dominio;
+
+import java.io.Serializable;
+
+public class Cidade implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private Integer codigo;
+	private String nome;
+	private String uf;
+	
+	public Cidade(){
+		
+	}
+	
+	public Cidade(Integer codigo, String nome, String uf){
+		this.codigo = codigo;
+		this.nome = nome;
+		this.uf = uf;
+	}
+	
+	public Integer getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(Integer codigo) {
+		this.codigo = codigo;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getUf() {
+		return uf;
+	}
+	public void setUf(String uf) {
+		this.uf = uf;
+	}
+	
+	public String toString(){
+		return nome;
+	}
+}
--- src/main/java/br/com/diegomota/cursojsf/mb/ControleAulaConversores.java
+++ src/main/java/br/com/diegomota/cursojsf/mb/ControleAulaConversores.java
@@ -1,7 +1,9 @@
 package br.com.diegomota.cursojsf.mb;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
@@ -9,13 +11,27 @@ import javax.faces.context.FacesContext;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import br.com.diegomota.cursojsf.conversores.ConverterCidade;
+import br.com.diegomota.cursojsf.dominio.Cidade;
+
 @Component("controleAulaConversores")
 @Scope("request")
 public class ControleAulaConversores {
 	private Calendar nascimento;
+	
+	private Cidade cidade;
 
+	private List<Cidade> listaCidades = new ArrayList<>();
+	
+	private ConverterCidade converterCidade;
+
+	
 	public ControleAulaConversores(){
 		nascimento = Calendar.getInstance();
+		listaCidades.add(new Cidade(1, "São Paulo", "SP"));
+		listaCidades.add(new Cidade(2, "Porto Alegre", "RS"));
+		listaCidades.add(new Cidade(3, "Passo Fundo", "RS"));
+		converterCidade = new ConverterCidade(listaCidades);
 	}
 	
 	public String entrarForm() {
@@ -27,7 +43,8 @@ public class ControleAulaConversores {
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		FacesMessage msg = new FacesMessage("Informe uma data valida!");
 		if (nascimento != null){
-			msg = new FacesMessage("Nascimento: "+sdf.format(nascimento.getTime()));
+			msg = new FacesMessage("Nascimento: "+sdf.format(nascimento.getTime()) +
+					" Cidade: "+cidade.getNome() + " UF: "+cidade.getUf());
 		} 
 		FacesContext.getCurrentInstance().addMessage("", msg);
 		nascimento = null;
@@ -41,4 +58,30 @@ public class ControleAulaConversores {
 	public void setNascimento(Calendar nascimento) {
 		this.nascimento = nascimento;
 	}
+
+	public Cidade getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(Cidade cidade) {
+		this.cidade = cidade;
+	}
+
+	public List<Cidade> getListaCidades() {
+		return listaCidades;
+	}
+
+	public void setListaCidades(List<Cidade> listaCidades) {
+		this.listaCidades = listaCidades;
+	}
+
+	public ConverterCidade getConverterCidade() {
+		return converterCidade;
+	}
+
+	public void setConverterCidade(ConverterCidade converterCidade) {
+		this.converterCidade = converterCidade;
+	}
+	
+	
 }
